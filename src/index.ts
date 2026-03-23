c3d0cbdd298ae157dcec75468a4ade91737ae759
Shubham Makkar <13jerry02@gmail.com>
2026-03-23 10:06:39 +0530

    fix: resolve merge conflict in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import listLanguages from './tools/listLanguages';
 import { Tool } from './types';
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
+import { createServer } from './server.js'
 
 const tools: Tool[] = [
   listLanguages
 ];
 
-export default tools;
+async function main() {
+    const server = createServer()
+    const transport = new StdioServerTransport()
+
+    await server.connect(transport)
+    export default tools;
+}
+
+main()
